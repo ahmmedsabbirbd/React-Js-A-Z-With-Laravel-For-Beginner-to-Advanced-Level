030e63b70ea20f3ae4575a40afbad2cedff8b7bf
Sabbir Ahmmed <ahmmedsabbirbd@gmail.com>
2023-04-05 14:51:41 +0600

    js function complete

---
+++ javascripts-basic/javascript_function.js
@@ -0,0 +1,66 @@
+/*
+    1==============> function declaration // function call
+
+    function aboutMe(name) {
+        console.log(`my name is ${name}`);
+    }
+
+    aboutMe("Sabbir");
+*/ 
+
+/*
+    2==============> deafult parameters
+    
+
+
+    function clacuSum(a = 0, b = 0) {
+        return a + b;
+    }
+
+    console.log(clacuSum(2, 3));
+ */
+
+/*
+    3==============> function expresion
+
+    console.log(clacuSum(2, 3));
+    function clacuSum(a = 0, b = 0) {
+        return a + b;
+    }
+
+
+    const calSumExpression = function(a, b) {
+        return a + b;
+    }
+    console.log(calSumExpression(2, 4));
+*/
+
+/*
+    4==============>  array function
+
+
+    const calSumArray = (a, b) =>  a + b;
+   
+    const calSumExpressionTwo = (a, b) => {
+        return a + b;
+    }
+    console.log(calSumArray(2, 4));
+*/
+
+/*
+    5==============>  rest parameter
+
+    function mulipleParameter(...arg) {
+        let sum = 0;
+        for(let n in arg) {
+            sum +=arg[n]; 
+        }    
+        return sum;
+    }
+    console.log(mulipleParameter(2, 32, 45));
+*/
+
+/*
+    6==============> 
+
+*/ 
